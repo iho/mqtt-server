2749ec60c7cd5aed2c1502f80b29bb45c075b682
Ihor Horobets <ihor@horobets.me>
2021-01-07 15:31:25 +0200

    Fix config and migration.

--- migrations/20210107144530-create_message_table.sql
+++ migrations/20210107144530-create_message_table.sql
@@ -1,9 +1,8 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS messages (
-    id      INTEGER      AUTO_INCREMENT NOT NULL,
-    name    TEXT                        NOT NULL,
-    message TEXT                        NOT NULL,
-    PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    message TEXT NOT NULL
 );
 -- +migrate Down
 DROP TABLE messages;
